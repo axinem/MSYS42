5773269f1135ed43ee4d5a554fc7b8ed033b05af
criesri <145673159+criesri@users.noreply.github.com>
2024-09-17 17:27:11 +0800

    added admin page

--- MSYS42Project/BalayBaler/admin.py
+++ MSYS42Project/BalayBaler/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
-
-from django.contrib import admin
-from .models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
